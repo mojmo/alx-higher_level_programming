9fe80f25db1363fb7a46953d114130bbe212029b
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-01 00:38:18 +0200

    handle add at begining and add at end cases

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,28 +11,36 @@
 */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *slow, *fast, *new_node;
+	listint_t *slow, *fast;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	new_node->n = number;
+	new_node->next = NULL;
 
 	if (head == NULL)
-		return (NULL);
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
 	slow = *head;
 	fast = (*head)->next;
 
 	while (slow != NULL && fast != NULL)
 	{
-		if (number > slow->n && number < fast->n)
+		if (number >= slow->n && number <= fast->n)
 		{
-			new_node = malloc(sizeof(listint_t));
-			new_node->n = number;
 			slow->next = new_node;
 			new_node->next = fast;
 			return (new_node);
 		}
+		if (number > fast->n && fast->next == NULL)
+			fast->next = new_node;
 
 		slow = slow->next;
 		fast = fast->next;
 	}
 
+	free_listint(new_node);
 	return (NULL);
 }
