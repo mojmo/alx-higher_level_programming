3ad998833173916d7c9e8ff49343ad0b3c05291c
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-02 23:17:34 +0200

    fix print format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if (args_len == 0):
         print("0 arguments.")
     else:
-        print("{} argument:".format(args_len))
+        print("{} argument{}:".format(args_len, 's' if args_len > 1 else ''))
 
         for arg in range(1, args_len + 1):
             print("{}: {}".format(arg, argv[arg]))
