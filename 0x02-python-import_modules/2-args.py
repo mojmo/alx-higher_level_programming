41657d3df327d2ab8b7d807002f29380374753bb
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-02 22:58:16 +0200

    Add '2. How to make a script dynamic' task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    args = argv[1:]
+    args_len = len(argv) - 1
+
+    if (args_len == 0):
+        print("0 arguments.")
+    else:
+        print("{} argument:".format(args_len))
+
+        for arg in range(1, args_len + 1):
+            print("{}: {}".format(arg, argv[arg]))
