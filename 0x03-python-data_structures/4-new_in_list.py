e0c7e6306a056af24e45ec38df7ef6af5c7ae891
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-03 22:58:15 +0200

    Add '4. Replace in a copy' task

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """replaces an element in a list at a specific position
+       without modifying the original list
+    """
+
+    new_list = my_list.copy()
+    if idx >= 0 and idx <= len(my_list):
+        new_list[idx] = element
+
+    return new_list
