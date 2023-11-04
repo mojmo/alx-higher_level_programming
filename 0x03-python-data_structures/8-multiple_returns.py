a4b9c8002c574ca8bc096bc52569db37284a4fe4
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-04 16:31:11 +0200

    change return logic

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,7 @@
 def multiple_returns(sentence):
     """returns a tuple with the length of a string and its first character."""
 
-    new_tuple = len(sentence), sentence[0]
+    if sentence:
+        return (len(sentence), sentence[0])
 
-    if sentence == '':
-        new_tuple = (len(sentence), None)
-
-    return new_tuple
+    return (0, None)
