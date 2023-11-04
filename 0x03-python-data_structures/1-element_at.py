cddb2f68b9282b8a22dce413b33b6904777e1005
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-04 13:11:38 +0200

    fix return value issue

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@
 def element_at(my_list, idx):
     """retrieves an element from a list"""
 
-    if (idx > 0) and (idx < len(my_list)):
+    if (idx >= 0) and (idx < len(my_list)):
         return my_list[idx]
