88f2622f3bd1d46c19bfdf36cd39f89e526c11f4
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-04 17:49:49 +0200

    fix linting

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 
 def delete_at(my_list=[], idx=0):
     """deletes the item at a specific position in a list."""
-    
+
     if my_list and (idx >= 0) and (idx < len(my_list)):
         del my_list[idx]
-        
+
     return my_list
