0252f0617a85221876f82a46d3bf5ad057798ebe
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-04 13:15:54 +0200

    handle empty list case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,6 @@
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order."""
 
-    for i in range(1, len(my_list) + 1):
-        print("{:d}".format(my_list[-i]))
+    if my_list:
+        for i in range(1, len(my_list) + 1):
+            print("{:d}".format(my_list[-i]))
