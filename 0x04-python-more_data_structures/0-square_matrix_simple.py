5196cbb2ff53f72bb71de8df9b97d97b0a925294
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 18:39:38 +0200

    Add '0. Squared simple' task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """computes the square value of all integers of a matrix."""
+    
+    square_matrix = [[num ** 2 for num in row ]for row in matrix]
+    
+    return square_matrix
