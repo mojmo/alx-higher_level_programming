6946cbfa5df9b1cf4ed427af5c0bb14e905d852d
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 21:38:46 +0200

    fix linting

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@
 def best_score(a_dictionary):
     """returns a key with the biggest integer value."""
 
-    if a_dictionary == None:
-        return
+    if a_dictionary is None:
+        return None
 
-    max_key = max(a_dictionary, key = a_dictionary.get)
+    max_key = max(a_dictionary, key=a_dictionary.get)
 
     return max_key
