90b934c1f7ba0c4a531444a80d6b7b826b9a2842
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 18:54:14 +0200

    fix linting

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
     """computes the square value of all integers of a matrix."""
-    
-    square_matrix = [[num ** 2 for num in row ]for row in matrix]
-    
+
+    square_matrix = [[num ** 2 for num in row] for row in matrix]
+
     return square_matrix
