c1f38f86f0905dde6a001d5771c0e3c1a1fc8c8c
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 22:04:06 +0200

    handle duplicated best score value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    max_key = max(a_dictionary, key=lambda key: a_dictionary[key])
+    max_key = max(sorted(a_dictionary), key=lambda key: a_dictionary[key])
 
     return max_key
