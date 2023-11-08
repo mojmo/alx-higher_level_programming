70a141a2b21d3e386fbad5ceac36583660044a65
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 22:47:14 +0200

    change the code style

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,13 @@ def best_score(a_dictionary):
     """returns a key with the biggest integer value."""
 
     if a_dictionary is None:
-        return None
+        return
 
-    max_key = max(sorted(a_dictionary, key=lambda key: a_dictionary[key]))
+    max_val = 0
+    max_key = ''
 
+    for k, v in a_dictionary.items():
+        if v > max_val:
+            max_val = v
+            max_key = k
     return max_key
