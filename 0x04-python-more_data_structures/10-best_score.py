95c1f3fd98f19ebb490fa67037077cecb64ee761
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-08 21:33:39 +0200

    Add '10. Best score' task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """returns a key with the biggest integer value."""
+
+    if a_dictionary == None:
+        return
+
+    max_key = max(a_dictionary, key = a_dictionary.get)
+
+    return max_key
