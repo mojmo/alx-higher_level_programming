0fed42e3cb50daafc5273c8d4279c15d10bb0c8c
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-09 00:29:27 +0200

    fix linting

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    """returns the weighted average of all integers tuple (<score>, <weight>)"""
+    """returns the weighted average of all integers
+    tuple (<score>, <weight>)"""
 
     if len(my_list) == 0:
         return 0
-    
+
     weight = 0
     score = 0
 
     for tup in my_list:
-            score += tup[0] * tup[1]
-            weight += tup[1]
-    
-    # avg = [[weight ** 2 for weight in tup] for tup in my_list]
+        score += tup[0] * tup[1]
+        weight += tup[1]
 
     return score / weight
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
