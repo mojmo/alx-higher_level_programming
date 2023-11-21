ce7b2511ab0005ef9ef55a3919f01a8e0fab56c0
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-21 17:15:03 +0200

    Add '0. My first square' task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Represents a square.
+
+    This class serves as a basic template for creating square objects.
+    It currently does not have any specific attributes or methods defined.
+    """
+    pass
