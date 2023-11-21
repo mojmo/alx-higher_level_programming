3ac7ab0fd7ca62c7172d2d49931eab53d33cdc62
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-21 17:49:47 +0200

    Add '1. Square with size' task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+
+"""Defines the Square class for representing squares."""
+
+
+class Square:
+    """Represents a square with a given size."""
+
+    def __init__(self, size):
+        """Initializes a square with the specified size.
+
+        Args:
+            size (int): The size of the square.
+        """
+
+        self.__size = size
