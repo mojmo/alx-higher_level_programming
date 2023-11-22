6213a9b93344f1e60e28cb22192c64391e347d25
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-22 02:03:01 +0200

    Add tests folder

---
+++ 0x06-python-classes/tests/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define an empty class called Square"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    This class provides a starting point for creating square objects.
+    """
+    pass
