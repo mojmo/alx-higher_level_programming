67985a5b3b8fa71a9caed2f6924ca2a21ca2b95b
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-27 19:43:00 +0200

    fix exception handling

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -155,14 +155,18 @@ class Rectangle:
             rect_1 (Rectangle): The first rectangle.
             rect_2 (Rectangle): The second rectangle.
 
+        Raises:
+            TypeError: if either rect_1 or rect_2 is not
+            an instance of Rectangle.
+
         Returns:
             Rectangle: The rectangle with the larger area.
         """
 
         if not isinstance(rect_1, Rectangle):
-            TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
 
         if rect_1.area() >= rect_2.area():
             return rect_1
