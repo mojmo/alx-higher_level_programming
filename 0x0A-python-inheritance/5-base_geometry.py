7f889a0099a596d999039b3d098e71b4a4146502
mojtaba <mojtaba.mohamed187@gmail.com>
2023-12-04 17:10:39 +0200

    Add '6. Improve Geometry' task

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that defines the BaseGeometry class.
+Defines the BaseGeometry class.
 """
 
 
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+    """
+    def area(self):
+        """
+        Placeholder method for calculating the area of a geometry.
+        This method is meant to be overridden in derived classes.
+        """
+        raise Exception("area() is not implemented")
