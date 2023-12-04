c864c926fcb19652368e741c09564b663a4773de
mojtaba <mojtaba.mohamed187@gmail.com>
2023-12-04 17:03:51 +0200

    Add '5. Geometry module' task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+    This class does not implement any specific functionality.
+    """
+    pass
