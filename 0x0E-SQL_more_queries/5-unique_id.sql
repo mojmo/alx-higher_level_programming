14825419a346eff629512aadf3648bfff96794e2
mojtaba <mojtaba.mohamed187@gmail.com>
2024-01-17 23:19:49 +0200

    Add '5. Unique ID' task

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Creates the table unique_id on a MySQL server.
+-- id INT with the default value 1 and must be unique.
+
+CREATE TABLE IF NOT EXISTS `unique_id` (
+    `id`   INT  DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
+);
