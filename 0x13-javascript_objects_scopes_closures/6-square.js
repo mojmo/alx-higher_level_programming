b11be5537782e1662d85df96a1005cc22f8b9058
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-13 21:54:51 +0200

    replace 'c' char with 'c' variable

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends require('./5-square.js') {
     } else {
       for (let i = 0; i < this.height; i++) {
         let row = '';
-        for (let j = 0; j < this.width; j++) { row += 'c'; }
+        for (let j = 0; j < this.width; j++) { row += c; }
         console.log(row);
       }
     }
