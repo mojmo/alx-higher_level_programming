493b8a4d4e541e482bd8cc09100892fa44e055d4
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 15:09:44 +0200

    change the sql query to ignore case-sensitivity using BINARY method

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute("""SELECT *
                 FROM `states`
-                WHERE `name` = '{}'
+                WHERE BINARY(`name`) = '{}'
                 ORDER BY id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
 
