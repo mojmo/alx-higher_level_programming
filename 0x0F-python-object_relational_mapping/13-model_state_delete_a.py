bef23cc1f050be91131f418b525e885c490f38aa
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 20:48:55 +0200

    Add Base database schema

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,7 +8,7 @@ Usage: ./script_name.py mysql_username mysql_password database_name
 """
 
 import sys
-from model_state import State
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     # Create a connection to the MySQL database using the provided credentials
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
