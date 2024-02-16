6b92f50faf28bd994351b1995274dff2ed85b07a
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 14:41:17 +0200

    Add task '1. Filter states'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+This script connects to a MySQL database and fetches all records from
+a table named 'states' where the name of the state starts with the
+letter 'N'.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    # Connect to the MySQL database using the provided credentials
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # Create a cursor object to execute SQL queries
+    cur = conn.cursor()
+    cur.execute("""SELECT *
+                FROM `states`
+                WHERE `name` LIKE 'N%'
+                ORDER BY id ASC""")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
