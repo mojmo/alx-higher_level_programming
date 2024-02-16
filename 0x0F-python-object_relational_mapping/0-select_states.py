13242a5289590dc266435939579567ee59cef5dd
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 14:09:49 +0200

    make code unexecutable when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,22 +9,23 @@ arguments: MySQL username, password, and database name.
 import MySQLdb
 from sys import argv
 
-# Connect to the MySQL database using the provided credentials
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3]
-)
+if __name__ == 'main':
+    # Connect to the MySQL database using the provided credentials
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-# Create a cursor object to execute SQL queries
-cur = conn.cursor()
-cur.execute("SELECT * FROM `states` ORDER BY id ASC")
-query_rows = cur.fetchall()
+    # Create a cursor object to execute SQL queries
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+    query_rows = cur.fetchall()
 
-for row in query_rows:
-    print(row)
+    for row in query_rows:
+        print(row)
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
