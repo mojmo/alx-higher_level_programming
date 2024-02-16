52e54bd352ba92d81bf8fffbfca53142dffadb35
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 13:53:49 +0200

    Add task '0. Get all states'

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This script connects to a MySQL database and fetches all records
+from a table named 'states'. It requires three command-line
+arguments: MySQL username, password, and database name.
+"""
+
+import MySQLdb
+from sys import argv
+
+# Connect to the MySQL database using the provided credentials
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3]
+)
+
+# Create a cursor object to execute SQL queries
+cur = conn.cursor()
+cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+query_rows = cur.fetchall()
+
+for row in query_rows:
+    print(row)
+
+cur.close()
+conn.close()
