180a971dc2a07aa73e9d4c25aa2a19f35a194268
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-16 20:09:28 +0200

    remove unnecessary var

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,7 +8,7 @@ Usage: ./script_name.py mysql_username mysql_password database_name
 """
 
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -16,11 +16,9 @@ if __name__ == "__main__":
     # Create a connection to the MySQL database using the provided credentials
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    # Create the database schema
-    Base.metadata.create_all(engine)
 
     # Create a session to interact with the database
-    Session = sessionmaker(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     # Fetch all states whose names contain the letter 'a' from the database
