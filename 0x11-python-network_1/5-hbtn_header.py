ee8fc36e2e64a94b46661265b02efb4fb6c66b1d
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-01 21:07:53 +0200

    Add task '5. Response header value #1'

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This script sends an HTTP GET request to a specified URL using the
+requests library and prints the value of the 'X-Request-Id' header
+from the response.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = argv[1]
+
+    res = requests.get(url)
+
+    print(f"{res.headers['X-Request-Id']}")
