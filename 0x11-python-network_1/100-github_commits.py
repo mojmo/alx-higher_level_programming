6441611d01dde1a4e4eadad3e2a3523d6478dab9
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-02 00:24:25 +0200

    remove unnecessary library

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,19 +3,13 @@
 """This script retrieves the latest 10 commit SHAs and their authors
 from a GitHub repository.
 
-Arguments:
-- <repository> (str): The name of the GitHub repository.
-- <owner> (str): The owner or organization of the GitHub repository.
-
 Usage: ./100-github_commits.py <repository> <owner>
 """
 
 from sys import argv
 import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-
     repo = argv[1]
     owner = argv[2]
 
