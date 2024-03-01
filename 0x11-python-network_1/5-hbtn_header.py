9dcc70ca8c1180a885cdfea4a1c8f7e6cb6081e0
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-01 22:03:28 +0200

    change a var name

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""This script sends an HTTP GET request to a specified URL using the
-requests library and prints the value of the 'X-Request-Id' header
-from the response.
+"""Prints the value of the 'X-Request-Id' header from the response.
 
 Usage: ./5-hbtn_header.py <URL>
 """
-import sys
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    res = requests.get(url)
-    print(res.headers.get("X-Request-Id"))
+    r = requests.get(argv[1])
+    print(r.headers.get("X-Request-Id"))
