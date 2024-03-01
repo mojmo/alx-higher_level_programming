3fd2d04d6741f37254544642f2cbad5749b2841b
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-01 21:42:47 +0200

    fix linting

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 """This script sends an HTTP GET request to a specified URL using the
 requests library and prints the value of the 'X-Request-Id' header
 from the response.
 """
-
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-
     url = sys.argv[1]
 
     res = requests.get(url)
