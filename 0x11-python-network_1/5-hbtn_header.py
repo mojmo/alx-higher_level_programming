92a62222a5e33a294bc9542ca9a65da1156dbc24
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-01 21:59:07 +0200

    edit docstring

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,6 +2,8 @@
 """This script sends an HTTP GET request to a specified URL using the
 requests library and prints the value of the 'X-Request-Id' header
 from the response.
+
+Usage: ./5-hbtn_header.py <URL>
 """
 import sys
 import requests
