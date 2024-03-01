60fb8464423334dbd35d9c6a8f80e637d6103e99
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-02 00:48:00 +0200

    get rid of some variables

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,13 +8,16 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    res = requests.get(f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits")
+    res = requests.get("https://api.github.com/repos/{}/{}/commits".format(
+        argv[2], argv[1]
+    ))
     json_res = res.json()
 
     try:
         for i in range(10):
-            author = json_res[i].get("commit").get("author").get("name")
-            sha = json_res[i].get("sha")
-            print(f'{sha}: {author}')
+            print('{}: {}'.format(
+                json_res[i].get("sha"),
+                json_res[i].get("commit").get("author").get("name")
+            ))
     except IndexError:
         pass
