5a23e973c78a3c5ff994013dfd037df0e0ecefe0
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-01 19:02:41 +0200

    use tab instead of spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     with urlopen("https://alx-intranet.hbtn.io/status") as res:
         res_body = res.read()
         print('Body response:')
-        print(f'    - type: {type(res_body)}')
-        print(f'    - content: {res_body}')
-        print(f'    - utf8 content: {res_body.decode("UTF-8")}')
+        print(f'\t- type: {type(res_body)}')
+        print(f'\t- content: {res_body}')
+        print(f'\t- utf8 content: {res_body.decode("UTF-8")}')
