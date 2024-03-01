347ce1f1862daea08ea10f36347a5cf915cd9bf1
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-02 00:42:07 +0200

    fix linting

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-
 """This script retrieves the latest 10 commit SHAs and their authors
 from a GitHub repository.
 
 Usage: ./100-github_commits.py <repository> <owner>
 """
-
 from sys import argv
 import requests
 
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-    res = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+    res = requests.get(f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits")
     json_res = res.json()
 
     try:
