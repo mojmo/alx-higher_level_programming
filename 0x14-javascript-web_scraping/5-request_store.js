d4b15d204dad877048ec400c51d0fe6b4d937de1
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-26 19:57:08 +0200

    Add task '5. Loripsum'

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) { console.log(err); }
+  });
+});
