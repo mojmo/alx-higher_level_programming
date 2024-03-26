c051797bb11c73cf0bcc9bf76ca39765e0a56289
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-26 15:09:50 +0200

    Add task '0. Readme'

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const filePath = process.argv[2];
+
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
+});
