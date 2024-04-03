8640b5d58e1ab9dcfac03c9cacc3edae923777a3
mojtaba <mojtaba.mohamed187@gmail.com>
2024-04-03 23:56:08 +0200

    Add task '9. Say Hello'

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+
+$('document').ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
